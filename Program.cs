88114a72617b40f4a72c3fe0b440d4ce2f13294a
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-07-18 10:38:21 +0100

    Lesson 1 Output

--- Program.cs
+++ Program.cs
@@ -4,16 +4,44 @@
 // dotnet build
 
 
-namespace MyFirstProgram
+// LESSON 1
+
+namespace Output
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // ESCAPE SEQUENCES
+            // New Line 
+            /* Also the method Write()
+             on the Console Object is for 
+            in line while the WriteLine() 
+            is for a creating a line 
+            for each output */
 
-            Console.WriteLine("Hello Dunya!"); // output on the console
-            Console.Beep(); // makes console beep
-            Console.ReadKey(); // press a key to exit the console
+            Console.Write("Hello\nWorld");
+            Console.Write("Hello\nDunya");
+
+            // Backspace
+            Console.WriteLine("Hello\bDunya");
+
+            // Form Feed
+            Console.WriteLine("Name:\fFahm");
+
+            // Carriage Return
+            Console.WriteLine("Hello\rDunya");
+
+
+            // Single and double quote
+            Console.WriteLine("\"Hello\" \'Dunya\'");
+
+            // Vertical Tab and Horizontal Tab
+            Console.WriteLine("Hello \v You");
+            Console.WriteLine("Hello \t You");
+
+            Console.Beep();
+            Console.ReadKey();
         }
     }
 }
