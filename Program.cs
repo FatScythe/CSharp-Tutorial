07972ea8cb8b12b4cbba3a4f8f64ffcb7aeaf49b
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-07-18 10:23:32 +0100

    My first Program on C#

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// dotnet new console -o app
+// dutnet run
+// dotnet build
+
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello Dunya!"); // output on the console
+            Console.Beep(); // makes console beep
+            Console.ReadKey(); // press a key to exit the console
+        }
+    }
+}
