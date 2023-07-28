7419df0592c9c1348dc9ddac53c596f99693b8c4
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-07-28 16:01:19 +0100

    Creating upstream for lesson 5

--- Program.cs
+++ Program.cs
@@ -1,21 +1,13 @@
-﻿// LESSON 3
+﻿// LESSON 5
 
-namespace Constant
+namespace UserInput
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /* To make a variable constant through out
-            it's lifecycle add the 'const' keyword b4
-            the declaration of a variable */
-
-            const double pi = 3.1423;
-            // Now reassigning it would no longer work
-            // pi = 22;
 
 
-            Console.WriteLine(pi);
             Console.ReadKey();
         }
     }
