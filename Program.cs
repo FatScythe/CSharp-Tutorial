ac1c7012008eae44aca49193069d5a492bbf11ec
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-07-28 10:19:22 +0100

    lesson 3 - Constant

--- Program.cs
+++ Program.cs
@@ -1,34 +1,21 @@
-﻿namespace Variables
+﻿// LESSON 3
+
+namespace Constant
 {
     class Program
     {
         static void Main(string[] args)
         {
+            /* To make a variable constant through out
+            it's lifecycle add the 'const' keyword b4
+            the declaration of a variable */
 
-            int x; // Initialization 
-            x = 123; // Declaration
-
-            int y = 321; // Initialization + Declaration
-
-            int z = x + y;
-
-            // float height = 200.59; // Float is used for less precise decimals 
-            double height = 200.59; // Float is used for precise decimals 
-                                    // decimal height = 200.59; // Float is used for more precise decimals 
-
-            string name = "Fahm"; // Stores Strings
-            char symbol = '@'; // Stores Character
-            bool isAlive = true; // Stores true or false
-            string userName = symbol + name;
+            const double pi = 3.1423;
+            // Now reassigning it would no longer work
+            // pi = 22;
 
 
-            Console.WriteLine(z);
-            Console.WriteLine("Your name is " + name);
-            Console.WriteLine("Your height is " + height + "cm");
-            Console.WriteLine("Are you alive? " + isAlive);
-            Console.WriteLine("Your username is: " + userName);
-
-
+            Console.WriteLine(pi);
             Console.ReadKey();
         }
     }
