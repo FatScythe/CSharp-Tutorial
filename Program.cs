7d9d4d42ea663bec9351a7568acddb9b58a79fa4
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-07-28 09:48:08 +0100

    Lesson 2

--- Program.cs
+++ Program.cs
@@ -1,19 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-// dotnet new console -o app
-// dutnet run
-// dotnet build
-
-
-namespace MyFirstProgram
+﻿namespace Variables
 {
     class Program
     {
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello Dunya!"); // output on the console
-            Console.Beep(); // makes console beep
-            Console.ReadKey(); // press a key to exit the console
+            int x; // Initialization 
+            x = 123; // Declaration
+
+            int y = 321; // Initialization + Declaration
+
+            int z = x + y;
+
+            // float height = 200.59; // Float is used for less precise decimals 
+            double height = 200.59; // Float is used for precise decimals 
+                                    // decimal height = 200.59; // Float is used for more precise decimals 
+
+            string name = "Fahm"; // Stores Strings
+            char symbol = '@'; // Stores Character
+            bool isAlive = true; // Stores true or false
+            string userName = symbol + name;
+
+
+            Console.WriteLine(z);
+            Console.WriteLine("Your name is " + name);
+            Console.WriteLine("Your height is " + height + "cm");
+            Console.WriteLine("Are you alive? " + isAlive);
+            Console.WriteLine("Your username is: " + userName);
+
+
+            Console.ReadKey();
         }
     }
 }
