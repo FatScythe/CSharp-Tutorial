adeb26da88c354abd18b2830909f1ff50b4f0669
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-07-28 15:59:42 +0100

    TypeCasting

--- Program.cs
+++ Program.cs
@@ -1,21 +1,35 @@
-﻿// LESSON 3
+﻿// LESSON 4
 
-namespace Constant
+namespace TypeCasting
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /* To make a variable constant through out
-            it's lifecycle add the 'const' keyword b4
-            the declaration of a variable */
+            // TypeCasting - When converting a data type 
+            // to another data type
+            // Useful - When receiving input 
+            // or when you need to use a different data type
+            double a = 123 + 0.223;
+            int b = Convert.ToInt32(a);
 
-            const double pi = 3.1423;
-            // Now reassigning it would no longer work
-            // pi = 22;
 
+            String c = "$";
+            char d = Convert.ToChar(c);
 
-            Console.WriteLine(pi);
+            String e = "true"; // string --> Boolean "true" === True, "false" === False
+            bool f = Convert.ToBoolean(e);
+
+            int g = 0; // int --> Boolean 0 === false, >0 =true
+            bool h = Convert.ToBoolean(g);
+            Console.WriteLine(h);
+
+            // Console.WriteLine(b.GetType());
+            // Console.WriteLine(d.GetType());
+            // Console.WriteLine(f.GetType());
+            Console.WriteLine(h.GetType());
+            Console.ReadKey();
+
             Console.ReadKey();
         }
     }
