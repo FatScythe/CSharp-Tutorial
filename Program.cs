8cb10d235c09719d5f90cd5b625d3bde0b3ce630
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-07-30 05:18:54 +0100

    Math Class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// LESSON 7
+using System;
 
 namespace MathClass
 {
@@ -6,7 +7,18 @@ namespace MathClass
     {
         static void Main(string[] args)
         {
+            double x = 3.99;
+            double y = 5;
 
+            // double a = Math.Pow(x, 3);
+            // double b = Math.Sqrt(x);
+            // double c = Math.Abs(x); // How far a number is from zero
+            // double d = Math.Round(x, 1); // Round down to 1 d.p
+            // double e = Math.Ceiling(x); // Rounds Up
+            // double f = Math.Floor(x); // Rounds Down
+            double g = Math.Max(x, y); // The highest numeber between 2 digits
+
+            Console.WriteLine(g);
             Console.ReadKey();
         }
     }
