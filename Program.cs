861dc5a3d2088d557dc297f83fea0ea9633b4a66
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-07-30 04:40:19 +0100

    User Input

--- Program.cs
+++ Program.cs
@@ -7,6 +7,16 @@ namespace UserInput
         static void Main(string[] args)
         {
 
+            Console.WriteLine("What is your name ?");
+
+            string? name = Console.ReadLine();
+
+            Console.WriteLine("What is your age ?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine($"You are {age} year old");
+
 
             Console.ReadKey();
         }
