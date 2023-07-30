a1df9025553922a8bf639004def7af1936402177
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-07-30 05:20:14 +0100

    Setting Upstream

--- Program.cs
+++ Program.cs
@@ -1,24 +1,13 @@
-﻿// LESSON 7
+﻿// LESSON 8
 using System;
 
-namespace MathClass
+namespace RandomNum
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double x = 3.99;
-            double y = 5;
 
-            // double a = Math.Pow(x, 3);
-            // double b = Math.Sqrt(x);
-            // double c = Math.Abs(x); // How far a number is from zero
-            // double d = Math.Round(x, 1); // Round down to 1 d.p
-            // double e = Math.Ceiling(x); // Rounds Up
-            // double f = Math.Floor(x); // Rounds Down
-            double g = Math.Max(x, y); // The highest numeber between 2 digits
-
-            Console.WriteLine(g);
             Console.ReadKey();
         }
     }
