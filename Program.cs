62ca5e391cf668d37a1fa8c95f8c6ea7e5e01acb
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-07-30 05:05:04 +0100

    Upstream

--- Program.cs
+++ Program.cs
@@ -1,30 +1,11 @@
-﻿// LESSON 6
+﻿// LESSON 7
 
-namespace ArithOp
+namespace MathClass
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int friends = 10;
-            // friends = friends + 1;
-            // friends += 2;
-            // friends++;
-
-            // friends = friends - 1;
-            // friends -= 2;
-            // friends--;
-
-            // friends = friends * 2;
-            // friends *= 2;
-
-            friends = friends / 2;
-            friends /= 2;
-
-            int remainder = friends % 3;
-
-            Console.WriteLine(remainder);
-            Console.WriteLine(friends);
 
             Console.ReadKey();
         }
