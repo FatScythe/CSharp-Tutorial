cb5a516d5a9ab48493876cc5c4bdbd58b18a7ffd
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-07-31 05:07:43 +0100

    Random Numbers

--- Program.cs
+++ Program.cs
@@ -7,7 +7,25 @@ namespace RandomNum
     {
         static void Main(string[] args)
         {
+            // Generating Random Number
+            Random random = new Random();
+            // .Next() returns a random integer number btw
+            // Btw 0 - >2billion
 
+            // int num = random.Next();
+
+            // Passing 2values in it limit its value
+
+            // Random 3 number from 3 die;
+            int die1 = random.Next(1, 7);
+            int die2 = random.Next(1, 7);
+            int die3 = random.Next(1, 7);
+
+            // .NextDouble() returns a random number >= 0,  <1
+            double num2 = random.NextDouble();
+
+            Console.WriteLine(die1); ;
+            Console.WriteLine(num2 * 100);
             Console.ReadKey();
         }
     }
