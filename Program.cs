f1982be00fbc67635c816a10b09d6a423bab1a4e
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-08-05 08:25:09 +0100

    Arrays

--- Program.cs
+++ Program.cs
@@ -7,7 +7,31 @@ namespace Arrays
         static void Main(string[] args)
         {
             //Arrays - are variables that can store multiple values, there have fixed length
-            //         As no length might cause an error
+
+            // Method 1
+            //  As no length, might cause an error when accesing an element that doesnot exist
+            // string[] cars = { "Benz", "Audi", "Lexus" };
+
+            //Method 2
+            // As Length, No errors
+            // string[] cars = new string[4];
+
+            // cars[0] = "Benz";
+            // cars[1] = "Audi";
+            // cars[2] = "Lexus";
+
+            // Method 3
+            //  As no length, might cause an error when accesing an element that doesnot exist
+            // Exception error - Index out of range
+            string[] cars = new string[] { "Benz", "Audi", "Lexus" };
+
+            Console.WriteLine(cars[3]);
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+
 
             Console.ReadKey();
         }
