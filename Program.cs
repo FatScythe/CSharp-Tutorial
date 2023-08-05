d5a25556ab0d1e98187b68d2e62ef4cce5d28fc2
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-08-05 06:53:19 +0100

    For Loops

--- Program.cs
+++ Program.cs
@@ -7,9 +7,20 @@ namespace ForLoop
         static void Main(string[] args)
         {
             // ForLoop - repeats as code for a finite amount of time
+            for (int i = 1; i <= 10; i += 2)
+            {
+                Console.WriteLine(i);
+            }
 
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Happy New Year!!!");
 
             Console.ReadKey();
         }
     }
+
 }
