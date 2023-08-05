a2847c943c18694e8687f219b90be51df1ec5421
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-08-05 06:32:48 +0100

    While Loop

--- Program.cs
+++ Program.cs
@@ -6,8 +6,17 @@ namespace WhileLoop
     {
         static void Main(string[] args)
         {
+            // While Loop - repeats a code while a condition remains true
 
-            // while loop - repeats a code while a condition remains true
+            string? name = "";
+
+            while (name == "")
+            {
+                Console.WriteLine("What is your name ?");
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine("Hi " + name);
 
             Console.ReadKey();
         }
