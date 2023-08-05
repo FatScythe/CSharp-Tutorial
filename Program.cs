c081efe02d0d665bd97ced7f428e924c811feae8
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-08-05 06:26:23 +0100

    Setting upstream

--- Program.cs
+++ Program.cs
@@ -1,27 +1,13 @@
-﻿// LESSON 12
+﻿// LESSON 13
 
-namespace LogicalOps
+namespace WhileLoop
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Logical Operators can be used to check if more than one operation is true or false
 
-            // && (AND)
-            // || (OR)
-
-            Console.WriteLine("What's the temperature outside: (C)");
-            double temp = Convert.ToDouble(Console.ReadLine());
-
-            if (temp >= 10 && temp <= 25)
-            {
-                Console.WriteLine("It's warm outside!");
-            }
-            else if (temp <= -50 || temp >= 50)
-            {
-                Console.WriteLine("DO NOT GO OUTSIDE!!!");
-            }
+            // while loop - repeats a code while a condition remains true
 
             Console.ReadKey();
         }
