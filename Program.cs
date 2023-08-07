e9cf6401bda826e2c25a663cd90615a18769b911
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-08-07 08:33:34 +0100

    Return Keyword

--- Program.cs
+++ Program.cs
@@ -1,41 +1,35 @@
-﻿// LESSON 18
+﻿// LESSON 19
 
-namespace Methods
+namespace Return
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // method - performs a section of code whenever it's called "invoked",
-            //          benefits - Reuse codes without rewriting it
-            string name = "Scythe";
-            int age = 99;
+            // Return Keyword - returns data back to where a method is invoked
+            double x;
+            double y;
+            double result;
 
-            SingHappyBirthday(name, age);
-            SingHappyBirthday(name, age);
-            SingHappyBirthday(name, age);
+            Console.WriteLine("----------------");
+            Console.WriteLine("Program to show case the Return keyword");
+            Console.WriteLine("----------------");
+
+            Console.Write("Enter first Number:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second Number:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            result = Multiply(x, y);
+
+            Console.WriteLine($"Result = {result}");
+
             Console.ReadKey();
         }
-        static void SingHappyBirthday(string name, int age)
+        static double Multiply(double x, double y)
         {
-            Console.WriteLine("Happy Birthday to you");
-            Console.WriteLine("Happy Birthday to you");
-            Console.WriteLine("Happy Birthday dear " + name);
-
-            Console.WriteLine("Happy Birthday");
-            Console.WriteLine("Happy Birthday");
-
-            Console.WriteLine("Happy Birthday to you");
-
-            Console.WriteLine("How old are you now?");
-            Console.WriteLine("How old are you now?");
-
-            Console.WriteLine("How old are you?, Dear " + name);
-            Console.WriteLine("I am " + age + " years old");
-
-            Console.WriteLine("");
-            Console.WriteLine("");
-
+            return x * y;
         }
     }
 
