6581c314915ada0662eeb05541abc2091c1897dc
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-08-07 08:38:55 +0100

    Method OverLoading

--- Program.cs
+++ Program.cs
@@ -1,35 +1,30 @@
-﻿// LESSON 19
+﻿// LESSON 20
 
-namespace Return
+namespace MethodOverloading
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Return Keyword - returns data back to where a method is invoked
-            double x;
-            double y;
-            double result;
+            // Method Overloading = methods can have the same name 
+            //                      but only differs with their parameters
+            //                      name + parameters = signature
+            //                      methods with same name must have a unique signature 
+            double total;
 
-            Console.WriteLine("----------------");
-            Console.WriteLine("Program to show case the Return keyword");
-            Console.WriteLine("----------------");
-
-            Console.Write("Enter first Number:");
-            x = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Enter second Number:");
-            y = Convert.ToDouble(Console.ReadLine());
-
-            result = Multiply(x, y);
-
-            Console.WriteLine($"Result = {result}");
+            // total = Multiply(2,3);
+            total = Multiply(2, 3, 5);
 
             Console.ReadKey();
         }
         static double Multiply(double x, double y)
         {
             return x * y;
+        }
+
+        static double Multiply(double x, double y, double z)
+        {
+            return x * y * z;
         }
     }
 
