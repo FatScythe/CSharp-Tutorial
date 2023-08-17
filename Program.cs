2c11dba0f0535e985dfcfdaead0a5b20be8ac58f
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-08-17 10:22:55 +0100

    Lists

--- Program.cs
+++ Program.cs
@@ -6,6 +6,34 @@ namespace List
     {
         static void Main(string[] args)
         {
+            // List = data structure that rep. a list of objects that can be accesed by index.
+            //        similar to an array, but can dynamically increase or decrease in size
+
+            List<int> list = new List<int>();
+            List<string> foods = new List<string>();
+
+            // To add to the list
+            foods.Add("Bread");
+            foods.Add("Rice");
+            foods.Add("Amala");
+            foods.Add("Beans");
+            foods.Add("Spaghetti");
+            foods.Add("Bread");
+
+            foods[2] = "Semo";
+            Console.WriteLine(foods[2]);
+            foods.Contains("Bread"); // Returns Boolean
+            foods.LastIndexOf("Bread"); //  Get last index of item
+            // foods.Clear(); // Clears the List
+            foods.Count(); // Gets the length of the list
+            foods.Remove("Beans"); //  Removes element from the list
+            string[] items = foods.ToArray(); // Converts list to array
+            // Console.WriteLine(items.GetType());
+
+            foreach (string food in foods)
+            {
+                // Console.WriteLine(food);
+            }
             Console.ReadKey();
 
         }
