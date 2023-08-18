c878da087239691af81d64942c4967b63a3ab6b5
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2023-08-18 09:42:59 +0100

    Not yet upstreamed

--- Program.cs
+++ Program.cs
@@ -1,46 +1,75 @@
-﻿// LESSON 40
+﻿// LESSON 41
 
-namespace ListOfObjects
+/* 
+git add . 
+git commit -m "Getters and Setters" 
+git push --set-upstream origin 41-Getters-Setters
+*/
+namespace GetSet
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // List = data structure that rep. a list of objects that can be accesed by index.
-            //        similar to an array, but can dynamically increase or decrease in size
 
-            List<Player> players = new List<Player>();
+            Car car = new Car("Tesla", 250);
 
-            Player player1 = new Player("Korede");
-            Player player2 = new Player("Gbenga");
-            Player player3 = new Player("Okiki");
+            // GettersSetters = at times you might want to prevent access to certain data
+            // e.g in this case I dont want the speed to be above 500
+            // I could use the 'Private' keyword to prevent this but i would no longer have access to the data
+            // Getter & Setter adds security to fields by encapsulation
+            // It also combine the aspect of fields and methods
+            // get accessor = used to return the property value
+            // set accessor = used to assign a new value
+            // value keyword = defines the value gotten from the constructor
 
-            players.Add(player1);
-            players.Add(player2);
-            players.Add(player3);
-            players.Add(new Player("Ola"));// or you can add them as anonymous object
+            // I should not be able to this
+            // car.speed = 100000000;
+            // I could make the the variable private 
+            // But if i need to change it i would have no way to
+            // Without creating a new instance of the class
+            car.Speed = 200000000;
+            // WIth Setter i can now set condition to prevent this
+            // Should i need to read what speed is, as it is private
+            // I can access the getters Speed
+            Console.WriteLine(car.Speed);
+            // Console.WriteLine(car);
 
-            foreach (Player player in players)
-            {
-                Console.WriteLine(player);
-            }
             Console.ReadKey();
-
         }
-
     }
 
-    class Player
+    class Car
     {
-        public string username;
-        public Player(string username)
+        private string name;
+        private int speed;
+
+        public Car(string name, int speed)
         {
-            this.username = username;
+            this.name = name;
+            Speed = speed;
         }
+
+        public int Speed
+        {
+            get { return speed; } // reed
+            set
+            {
+                if (value > 500)
+                {
+                    speed = 200;
+                }
+                else
+                {
+                    speed = value;
+                }
+            } // write
+        }
+
         public override string ToString()
         {
-            return username;
+            return $"This ia a {name}, with a speed of {speed}";
         }
-    }
 
+    }
 }
